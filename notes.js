fc56b36af52e5c9ebf81f00c4529eac704a8463d
nikhilmattu <nikhilmattu@gmail.com>
2017-12-05 14:46:36 -0800

    Code Improvements
    Added new features
    Improved UI

--- notes.js
+++ notes.js
@@ -1,88 +1,201 @@
 const storage = window.localStorage;
-const NOTES = 'notes';
+const NOTES = "myNotes";
+const ID = "notesId";
+const notes = JSON.parse(storage.getItem(NOTES) || "[]");
+let id = JSON.parse(storage.getItem(ID) || "0");
+const filters = {
+  COMPLETED: 'completed',
+  INCOMPLETE: 'incomplete',
+  ALL: 'all',
+};
+let currentFilter;
+
+class Note {
+  constructor(id, content, createdDate, lastEditedDate, completed = false) {
+    this.id = id;
+    this.content = content;
+    this.createdDate = createdDate;
+    this.lastEditedDate = lastEditedDate;
+    this.completed = completed;
+  }
+}
+
+const renderNotes = function () {
+  const container = document.getElementById("notesContainer");
+
+  while(container.firstChild) {
+    container.removeChild(notesContainer.firstChild);
+  }
+
+  notes.forEach(note => {
+    switch (currentFilter) {
+      case filters.ALL:
+        const newNote = createNote(note);
+        container.append(newNote);
+        break;
+      case filters.COMPLETED:
+        if(note.completed){
+          const newNote = createNote(note);
+          container.append(newNote);
+        }
+        break;
+      case filters.INCOMPLETE:
+        if(!note.completed){
+          const newNote = createNote(note);
+          container.append(newNote);
+        }
+        break;
+      default:
+        console.log("This should never happen!");
+    }
+  });
+}
+
+const createNote = function (note) {
+  const noteContainer = document.createElement("div");
+  noteContainer.className = "noteContainer";
+
+  const newNote = document.createElement("TEXTAREA");
+  newNote.className = "noteContent";
+  newNote.innerText = note.content;
+  newNote.id = `note${note.id}`;
+
+  newNote.addEventListener('blur', (e) =>{
+    saveNote(note.id);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.className = "deleteButton";
+
+  deleteButton.addEventListener('click', (e) => {
+    deleteNote(note.id);
+  });
+
+  const noteInfo = document.createElement("div");
+  noteInfo.className = "noteInfo";
+  noteInfo.innerText = note.completed ?
+  `Completed: ${note.lastEditedDate}
+  Created: ${note.createdDate}` :
+  `Last Edited: ${note.lastEditedDate}
+  Created: ${note.createdDate}`;
+
+  if(note.completed){
+    const incompleteButton = document.createElement("button");
+    incompleteButton.className = "incompleteButton";
+
+    incompleteButton.addEventListener('click', (e) => {
+      markIncomplete(note.id);
+    });
+    noteContainer.append(incompleteButton);
+  }
+
+  if(!note.completed){
+    const completeButton = document.createElement("button");
+    completeButton.className = "completeButton";
+
+    completeButton.addEventListener('click', (e) => {
+      markComplete(note.id);
+    });
+    noteContainer.append(completeButton);
+  }
+
+  noteContainer.append(deleteButton);
+  noteContainer.append(newNote);
+  noteContainer.append(noteInfo);
+
+  return noteContainer;
+}
+
+const saveNote = function(id) {
+  const note = document.getElementById(`note${id}`);
+  const noteContent = note.value;
+
+  // If note is emptied, assumption is being made that it is being deleted
+  if(noteContent === ""){
+    deleteNote(id);
+    return;
+  }
+
+  const newContent = noteContent.trim();
+
+  let index = notes.map(function(note){
+    return note.id;
+  }).indexOf(id);
+
+  notes[index].content = newContent;
+  notes[index].lastEditedDate = new Date().toUTCString();
+
+  note.value = newContent;
+  storage.setItem(NOTES, JSON.stringify(notes));
+}
+
+const deleteNote = function(id) {
+  let index = notes.map(function(note) {
+    return note.id;
+  }).indexOf(id);
+
+  notes.splice(index, 1);
+
+  storage.setItem(NOTES, JSON.stringify(notes));
+  renderNotes();
+}
+
+const markComplete = function(id) {
+  let index = notes.map(function(note) {
+    return note.id;
+  }).indexOf(id);
+
+  notes[index].completed = true;
+  notes[index].lastEditedDate = new Date().toUTCString();
+
+  storage.setItem(NOTES, JSON.stringify(notes));
+  renderNotes();
+}
+
+const markIncomplete = function(id) {
+  let index = notes.map(function(note) {
+    return note.id;
+  }).indexOf(id);
+
+  notes[index].completed = false;
+  notes[index].lastEditedDate = new Date().toUTCString();
+
+  storage.setItem(NOTES, JSON.stringify(notes));
+  renderNotes();
+}
+
+const setFilter = function(filter) {
+  let previousFilterButton = document.getElementById(currentFilter);
+  previousFilterButton.className = "";
+
+  let currentFilterButton = document.getElementById(filter);
+  currentFilterButton.className = "activeFilter";
+
+  currentFilter = filter;
+  renderNotes();
+}
 
 window.onload = function() {
-  var notes;
-  notes = JSON.parse(storage.getItem(NOTES) || "[]");
-
-  var renderNotes = function () {
-    let container = document.getElementById("notesContainer");
-
-    while(notesContainer.firstChild) {
-      notesContainer.removeChild(notesContainer.firstChild);
-    }
-
-    for(let note of notes) {
-      let noteContainer = document.createElement("div");
-      noteContainer.className = "noteContainer";
-
-      let newNote = document.createElement("TEXTAREA");
-      newNote.className = "note";
-      newNote.innerHTML = note;
-
-      let del = document.createElement("button");
-      del.innerHTML = "Delete";
-
-      newNote.addEventListener('click', (e) => {
-        e.preventDefault();
-        editNote(noteContainer, newNote, del, note);
-      })
-
-      del.addEventListener('click', (e) => {
-        e.preventDefault();
-        deleteNote(noteContainer, note);
-      });
-
-      noteContainer.append(newNote);
-      noteContainer.append(del);
-      container.append(noteContainer);
-    }
-  }
+  currentFilter = filters.ALL;
+  let filterButton = document.getElementById(filters.ALL);
+  filterButton.className = "activeFilter";
 
   renderNotes();
 
-  function deleteNote(element, note) {
-    let index = notes.indexOf(note);
+  const form = document.getElementById("createNoteForm");
 
-    if(index > -1)
-      notes.splice(index, 1);
+  const formSubmit = form.addEventListener("submit", (e) => {
+    e.preventDefault(); // Allows for form to be focused after submit
+    const content = document.getElementById("newNoteText").value;
+    const date = new Date().toUTCString();
+    const note = new Note(id, content.trim(), date, date);
 
+    id++;
+
+    notes.unshift(note);
     storage.setItem(NOTES, JSON.stringify(notes));
-
+    storage.setItem(ID, JSON.stringify(id));
+    form.reset();
     renderNotes();
-  }
-
-  function editNote(noteContainer, note, del, previous) {
-    if(noteContainer.childNodes.length < 3){
-      let edit = document.createElement("button");
-      edit.innerHTML = "Edit";
-
-      edit.addEventListener('click', (e) => {
-        let index = notes.indexOf(previous);
-        if(index > -1)
-          notes.splice(index, 1);
-
-        notes.unshift(note.value);
-        storage.setItem(NOTES, JSON.stringify(notes));
-        noteContainer.removeChild(edit);
-        renderNotes();
-      });
-
-      noteContainer.insertBefore(edit, del);
-    }
-  }
-
-  var formSubmit = document.querySelector('form').addEventListener('submit', (e) => {
-    const newNoteContent = document.getElementById("newNoteText").value;
-    e.preventDefault();
-
-    if(notes.length > 0)
-      notes.unshift(newNoteContent);
-    else
-      notes.push(newNoteContent);
-
-    storage.setItem(NOTES, JSON.stringify(notes));
-    renderNotes();
-    location.reload();
   });
 }
